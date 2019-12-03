6bb5483345daf056524e4aed2c1208ad5f2d0890
Glitch (tmonks-exercise-tracker) <none>
2019-12-03 19:35:38 +0000

    get users and add exercise endpoints

--- server.js
+++ server.js
@@ -20,9 +20,54 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post("/api/exercise/new-user", (req, res) => {
+// CREATE A NEW USER
+app.post("/api/exercise/new-user", async (req, res) => {
   console.log("New user received: " + req.body.username);
-    res.json({ username: req.body.username });
+  let newUser = new User({username: req.body.username});
+  try {
+    const results = await newUser.save();
+    res.json({ _id: results._id, username: results.username });
+  } catch (e) {
+    console.log(e);
+    res.send(e.code === 11000 ? "Username already exists" : "An error occurred");
+  }
+});
+
+// GET LIST OF USERS
+app.get("/api/exercise/users", async (req, res) => {
+  try {
+    const results = await User.find({}, { log: 0 });
+    console.log(results);
+    res.send(results);
+  } catch(e) {
+    console.log(e);
+    res.send("An error occurred");
+  }
+});
+
+// ADD NEW EXERCISE
+app.post("/api/exercise/add", async (req, res) => {
+  const user = await User.findById(req.body.userId);
+  console.log(user);
+  if(!user) {
+    res.send("userId " + req.body.userId + " not found");
+    return;
+  } 
+  const exercise = { 
+    description: req.body.description,
+    duration: req.body.duration
+  }
+  // if a date was provided, set it in the exercise
+  if(req.body.date) exercise.date = req.body.date
+  user.log.push(exercise);
+  
+  try {
+    await user.save();
+    res.json(user);
+  } catch(e) {
+    console.log(e);
+    res.send("Sorry, there was an error saving the exercise.");
+  }
 });
 
 // Not found middleware
@@ -56,6 +101,7 @@ const userSchema = new mongoose.Schema({
   _id: { type: String, default: shortid.generate },
   username: { type: String, required: true, unique: true },
   log: [{
+    _id: false,
     description: String,
     duration: Number,
     date: { type: Date, default: Date.now }
